f546e692b2dc76f29f43cf124d0f73768638a5c1
habib.Ur.Rehman <habib.dev.vr2@gmail.com>
2023-09-21 10:10:42 +0400

    Update auto discovery

--- src/LaraHtmlServiceProvider.php
+++ src/LaraHtmlServiceProvider.php
@@ -8,9 +8,13 @@ class LaraHtmlServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/larahtml.php',
+            'larahtml'
+        );
         $this->publishes([
-            __DIR__.'/config/larahtml.php' => config_path('larahtml.php')
+            __DIR__ . '/config/larahtml.php' => config_path('larahtml.php'),
         ]);
     }
 
