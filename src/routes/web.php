403cb4a3c701c5b0a93c8d231425a41081baca35
habib.Ur.Rehman <habib.dev.vr2@gmail.com>
2023-09-24 10:54:37 +0400

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,46 +3,47 @@
 use Illuminate\Support\Facades\Route;
 
 sacnGivenDir(base_path(config('larahtml.view_base_path')));
-function sacnGivenDir($directory) {
-
+function sacnGivenDir($directory)
+{
     $directory_stack = array($directory);
     $ignored_filename = array(
         '.git' => true,
         '.svn' => true,
         '.hg' => true,
     );
-    $file_list = array();
+
     while ($directory_stack) {
         $current_directory = array_shift($directory_stack);
         $files = scandir($current_directory);
+
         foreach ($files as $filename) {
             $pathname = $current_directory . DIRECTORY_SEPARATOR . $filename;
-            if (isset($filename[0]) && (
-                $filename[0] === '.' ||
+            if (isset($filename[0]) && ($filename[0] === '.' ||
                 $filename[0] === '_' ||
                 isset($ignored_filename[$filename])
-            )) 
-            {
+            )) {
                 continue;
-            }
-            else if (is_dir($pathname) === TRUE) {
+            } else if (is_dir($pathname) === TRUE) {
                 $directory_stack[] = $pathname;
             } else if (pathinfo($pathname, PATHINFO_EXTENSION) === 'php') {
-                $split_main_dir = explode('resources/views/',$pathname);
-
-                if(isset($split_main_dir[1])){
-                    appendRoute($split_main_dir[1]);
+                $split_main_dir = explode('resources/views/', $pathname)[0];
+                if (isset($split_main_dir)) {
+                    appendRoute($split_main_dir);
                 }
             }
         }
     }
 }
 
-function appendRoute($slug){
-    $removeBladePhpExtention = str_replace(".blade.php","",$slug);
-    $removeSlash = str_replace("/",".",$removeBladePhpExtention); 
-
-    Route::get($removeBladePhpExtention,function() use($removeSlash){
-        return view($removeSlash);
+function appendRoute($slug)
+{
+    $removeBladePhpExtention = str_replace(".blade.php", "", $slug);
+    //Route name
+    $getRouteName = substr($removeBladePhpExtention, strpos($removeBladePhpExtention, 'views') + strlen('views'));
+    //View name
+    $viewRaw = preg_replace('|\\\\|',  '.', $getRouteName);
+    echo "Route Name - " . $getRouteName . " View name - " . $viewRaw . "<br>";
+    Route::get($getRouteName, function () use ($viewRaw) {
+        return view($viewRaw);
     });
 }
